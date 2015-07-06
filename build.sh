0c59101b6d38b7076097d198b388075bec822056
Wenxuan Zhao <vince.zhaowenxuan@gmail.com>
2015-07-06 18:48:20 +0800

    fix redirection

--- build.sh
+++ build.sh
@@ -26,3 +26,6 @@ find dist/ -name "*.html" -exec sed -i "s#<body>#<body>${INJECT_HEADER}#g" '{}' 
 find dist/ -name "*.html" -exec sed -i "s#gitbook/images/favicon.ico#/img/favicon.ico#g" '{}' \;
 # WARNING!!! FOUC HACK!
 sed -i "s#e(\"head\")\.html(f)#e(\"head title\").html(s.find(\"title\").html()),e(\"head link[rel=prev]\").attr(\"href\", s.find(\"link[rel=prev]\").attr(\"href\")),e(\"head link[rel=next]\").attr(\"href\", s.find(\"link[rel=next]\").attr(\"href\"))#g" dist/gitbook/app.js
+
+# redirect old url
+cp -r redirect/* dist/
